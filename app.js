85914b1294eea0d54b1ee861a95ccbb72853683b
Lokeshbabu <m.s.lokeshbabu@accenture.com>
2017-04-20 18:03:47 +1000

    commiting the application with new flow

--- app.js
+++ app.js
@@ -23,14 +23,20 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 //create LUIS recognizer that points at our model and add it as a root '/' dialog
-var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/bd2a8c49-4154-4771-9127-2ac0f1e41e5e?subscription-key=8a6d7ac6787c4537aab3095d94985a35&timezoneOffset=0.0&verbose=true&q=';
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/a4140058-f613-4a34-bab0-a90a6e088f86?subscription-key=8a6d7ac6787c4537aab3095d94985a35&verbose=true&timezoneOffset=0.0&q=';
 var recognizer = new builder.LuisRecognizer(model);
 var dialog = new builder.IntentDialog({ recognizers: [recognizer]});
 bot.dialog('/', dialog);
 
 // bot.dialog('/', [
 //     function (session) {
-//         var msg = new builder.Message(session)
+//         builder.Prompts.text(session, 'Hi');
+//     }
+// ]);
+
+// bot.dialog('Hi', [
+//     function (session) {
+//             var msg = new builder.Message(session)
 //             .textFormat(builder.TextFormat.xml)
 //             .attachments([
 //                 new builder.HeroCard(session)
@@ -42,70 +48,96 @@ bot.dialog('/', dialog);
 //                     ])
 //                     .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle"))
 //             ]);
-//         //session.endDialog(msg);
+
+//             builder.Prompts.text(session, 'Hi! Type in the search for candidates ');
+//     },
+//         function (session, results) {
+//         session.endDialogWithResult(results);
 //     }
 // ]);
 
 // // // Add intent handlers
-dialog.matches('StartActivity',[
+dialog.matches('SearchCandidate',[
     function (session, args, next){
         // get all the entities 
-        session.send('Welcome! We are analyzing your message: \'%s\'', session.message.text);
-        var ActivityType = builder.EntityRecognizer.findEntity(args.entities, 'ActivityType');
-        var ActivityDuration = builder.EntityRecognizer.findEntity(args.entities, 'ActivityDuration');
-        var CandidateType = builder.EntityRecognizer.findEntity(args.entities, 'candidatetype');
-        var location = builder.EntityRecognizer.findEntity(args.entities, 'location');
-        var technology = builder.EntityRecognizer.findEntity(args.entities, 'technology'); 
+        session.send('Welcome! we are analyzing your message: \'%s\'', session.message.text);
+
+        var cities = builder.EntityRecognizer.findEntity(args.entities, 'cities');
+        var datetime = builder.EntityRecognizer.findEntity(args.entities, 'datetime');
+        var email = builder.EntityRecognizer.findEntity(args.entities, 'email');
+        var experienceLevel = builder.EntityRecognizer.findEntity(args.entities, 'experienceLevel');
+        var leadtime = builder.EntityRecognizer.findEntity(args.entities, 'leadtime'); 
+        var number = builder.EntityRecognizer.findEntity(args.entities, 'number');
+        var phonenumber = builder.EntityRecognizer.findEntity(args.entities, 'phonenumber');
+        var primarySkill = builder.EntityRecognizer.findEntity(args.entities, 'primarySkill');
+        var secondarySkills = builder.EntityRecognizer.findEntity(args.entities, 'secondarySkills');
 
         var candidateprofile = session.dialogData.candidate = {
-            ActivityType : ActivityType ? ActivityType.entity :null,
-            ActivityDuration : ActivityDuration ? ActivityDuration.entity : null,
-            CandidateType : CandidateType ? CandidateType.entity : null,
-            location : location ? location.entity : null,
-            technology : technology ? technology.entity : null
+            cities : cities ? cities.entity :null,
+            datetime : datetime ? datetime.entity : null,
+            email : email ? email.entity : null,
+            experienceLevel : experienceLevel ? experienceLevel.entity : null,
+            leadtime : leadtime ? leadtime.entity : null,
+            number : number ? number.entity : null,
+            phonenumber : phonenumber ? phonenumber.entity : null,
+            primarySkill : primarySkill ? primarySkill.entity : null,
+            secondarySkills : secondarySkills ? secondarySkills.entity : null
         }
-        if (candidateprofile.location){
-            console.log(candidateprofile.location);
-        }
-        
-        if (!candidateprofile.technology){
-            builder.Prompts.text(session, 'what technology candidate is required?');
+         
+        if (!candidateprofile.primarySkill){
+            builder.Prompts.text(session, 'what primary skill you are looking for?');
         } else {
             next();
         }
     },
     function (session, results, next){
-        var tech = session.dialogData.candidate;
+        var pskill = session.dialogData.candidate;
         //get the duration
         if (results.response){
-            tech.technology = results.response;
+            pskill.primarySkill = results.response;
         }
 
-        if (tech.technology && !tech.location) {
-            builder.Prompts.text(session, 'what location candidate is required ?');
+        if (pskill.primarySkill && !pskill.experienceLevel) {
+            builder.Prompts.text(session, 'what experience level candidate you are looking for ?');
         } else {
             next();
         }
     },
     function (session, results, next){
-        var loc = session.dialogData.candidate;
+
+        var explevel = session.dialogData.candidate;
+
         if (results.response){
-            loc.location = results.response;
+            explevel.experienceLevel = results.response;
         }
-        if (loc.location && loc.technology && !loc.ActivityDuration){
-            builder.Prompts.text(session, 'when candidate is required (lead time) eg in 1 week, in 3 months?');
+        if (explevel.experienceLevel && explevel.primarySkill && !explevel.leadtime){
+            builder.Prompts.text(session, 'when do you need this candidate to start! eg in 1 week, in 3 months or 1st April etc..?');
+        } else {
+            next();
+        }
+
+    },
+    function (session, results, next){
+
+        var ltime = session.dialogData.candidate;
+
+        if (results.response){
+            ltime.leadtime = results.response;
+        }
+        if (ltime.experienceLevel && ltime.primarySkill && ltime.leadtime && !ltime.cities){
+            builder.Prompts.text(session, 'which location candidate is required for?');
         } else {
             next();
         }
 
     },
     function (session, results){
-        var duration = session.dialogData.candidate;
+        var city = session.dialogData.candidate;
         if (results.response){
-            duration.ActivityDuration = results.response;
+            city.cities = results.response;
         }
-        if (duration.location && duration.technology && duration.ActivityDuration){
-            duration.address = session.message.address;
+        if (city.experienceLevel && city.primarySkill && city.leadtime && city.cities){
+            city.address = session.message.address;
             console.log("Initiate database connection");
             var Connection = require('tedious').Connection;  
             var config = {
@@ -165,7 +197,7 @@ dialog.matches('StartActivity',[
     }
 ]);
 
-dialog.matches('StopActivity', [
+dialog.matches('EmailCandidate', [
     function(session, args, next){
         session.send('Thanks for using the service. try searching resources any time: \'%s\'', session.message.text);
     }
